4af1946aba75de2ce20b9d2244193237f1131316
Rafael Neves <rafael.neves@info.ufrn.br>
2022-02-19 15:26:23 -0300

    Primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  interface Props {
+    id: number,
+    cor: string
+  }
+
+  const cartas: Array<Props> = [
+    {
+      id: 1,
+      cor: 'blue'
+    },
+    {
+      id: 2,
+      cor: 'blue'
+    },
+    {
+      id: 3,
+      cor: 'blue'
+    },
+    {
+      id: 4,
+      cor: 'blue'
+    },
+    {
+      id: 5,
+      cor: 'blue'
+    },
+    {
+      id: 6,
+      cor: 'blue'
+    },
+    {
+      id: 7,
+      cor: 'blue'
+    },
+    {
+      id: 8,
+      cor: 'blue'
+    },
+    {
+      id: 9,
+      cor: 'blue'
+    },
+  ];
+
+  const [bg, setBg] = useState(cartas);
+
+  const alteraCor = (data: {id: number, cor: string}, event: any) => {
+    console.log("Clicou no " + data.id, event.target);
+    //event.target.style.backgroundColor =  event.target.style.backgroundColor == 'blue' ? 'red' : 'blue';
+    bg[data.id].cor = 'red';
+    console.log(bg);
+  }
+
+  useEffect(() => {
+    setBg(bg);
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Jogo da memória</h1>
+      <div className="bloco">
+        {
+          bg.map(data => <div className="card" style={{backgroundColor: data.cor}} onClick={(event) => alteraCor(data, event)} id={"carta-"+ data.id.toString()}></div>)
+        }
+      </div>
     </div>
   );
 }
